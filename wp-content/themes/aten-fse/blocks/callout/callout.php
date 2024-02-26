cd8c960ebf9438cb844233e79c56470506af9582
Elise Frigoli <elise.frigoli@gmail.com>
2024-02-26 16:49:19 -0600

    Refactoring Callout block

--- wp-content/themes/aten-fse/blocks/callout/callout.php
+++ wp-content/themes/aten-fse/blocks/callout/callout.php
@@ -2,6 +2,8 @@
 /**
  * Callout Link Block Template.
  *
+ * @package aten-fse
+ *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
  * @param   bool $is_preview True during backend preview render.
@@ -11,8 +13,8 @@
  * @param   array $context The context provided to the block by the post or it's parent block.
  */
 
-if( isset( $block['data']['preview_image'] )  ) :    /* rendering in inserter preview  */
-    echo '<img src="'. $block['data']['preview_image'] .'" style="width:100%; height:auto;">';
+if ( isset( $block['data']['preview_image'] ) ) :    /* rendering in inserter preview  */
+	echo '<img src="' . esc_attr( $block['data']['preview_image'] ) . '" style="width:100%; height:auto;">';
 else :
 
 	// Support custom "anchor" values.
@@ -31,33 +33,41 @@ else :
 	}
 
 	// Load values and assign defaults.
-	$custom_title = get_field('title');
-	$custom_image = get_field('image');
-	$teaser =  get_field('teaser');
-	$referenced_post_link = get_field('referenced_post_link');
+	$custom_title         = get_field( 'title' );
+	$custom_image         = get_field( 'image' );
+	$teaser               = get_field( 'teaser' );
+	$referenced_post_link = get_field( 'referenced_post_link' );
 
-	if ($referenced_post_link) {
-		$permalink = get_permalink($referenced_post_link);
-		$has_featured_image = has_post_thumbnail($referenced_post_link);
+	if ( $referenced_post_link ) {
+		$permalink          = get_permalink( $referenced_post_link );
+		$has_featured_image = has_post_thumbnail( $referenced_post_link );
 	}
 
-	$title = !empty($custom_title) ? $custom_title : get_the_title($referenced_post_link);
-	$image_id = !empty($custom_image) ? $custom_image : ($has_featured_image ? get_post_thumbnail_id($referenced_post_link) : '');
+	$callout_title = ! empty( $custom_title ) ? $custom_title : get_the_title( $referenced_post_link );
+	$image_id      = ! empty( $custom_image ) ? $custom_image : ( $has_featured_image ? get_post_thumbnail_id( $referenced_post_link ) : '' );
 
 	?>
 
-	<div <?php echo $anchor; ?> class="l-gutter callout-link-component <?php if (!$image_id) { echo "callout-without-image "; }; echo esc_attr($class_name); ?>">
+	<div <?php echo esc_attr( $anchor ); ?> class="l-gutter callout-link-component 
+					<?php
+					if ( ! $image_id ) {
+						echo 'callout-without-image ';
+					} echo esc_attr( $class_name );
+					?>
+	">
 		<div class="callout-link-text">
-			<?php if ($referenced_post_link) : ?>
-				<a href="<?php echo esc_url($permalink); ?>">
-					<h2><?php echo esc_html($title); ?></h2>
+			<?php if ( $referenced_post_link ) : ?>
+				<a href="<?php echo esc_url( $permalink ); ?>">
+					<h2><?php echo esc_html( $callout_title ); ?></h2>
 				</a>
-				<hr class="gold" />
-				<?php if( $teaser ) { ?><div class="callout-teaser"><?php echo ( $teaser ); ?></div><?php } ?>
+				<?php
+				if ( $teaser ) {
+					?>
+					<div class="callout-teaser"><?php echo esc_html( $teaser ); ?></div><?php } ?>
 			<?php endif; ?>
 		</div>
 
-		<?php if ($image_id) : ?>
+		<?php if ( $image_id ) : ?>
 			<div class="callout-link-image">
 				<?php echo wp_get_attachment_image( $image_id, 'callout-link' ); ?>
 			</div>
