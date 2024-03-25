68828c634413a684c6813158585862251794e160
Elise Frigoli <elise.frigoli@gmail.com>
2024-03-25 09:39:36 -0500

    Refactoring Callout Block

--- wp-content/themes/aten-fse/blocks/callout-block/callout-block.php
+++ wp-content/themes/aten-fse/blocks/callout-block/callout-block.php
@@ -2,6 +2,8 @@
 /**
  * Callout Block Template.
  *
+ * @package aten-fse
+ *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
  * @param   bool $is_preview True during backend preview render.
@@ -11,8 +13,8 @@
  * @param   array $context The context provided to the block by the post or it's parent block.
  */
 
-if( isset( $block['data']['preview_image'] )  ) :    /* rendering in inserter preview  */
-    echo '<img src="'. $block['data']['preview_image'] .'" style="width:100%; height:auto;">';
+if ( isset( $block['data']['preview_image'] ) ) :    /* rendering in inserter preview  */
+	echo '<img src="' . esc_attr( $block['data']['preview_image'] ) . '" style="width:100%; height:auto;">';
 else :
 
 	// Support custom "anchor" values.
@@ -31,37 +33,31 @@ else :
 	}
 
 	// Load values and assign defaults.
-	$title = get_field('title');
-	$link = get_field('link');
-	$body =  get_field('body_text');
-	$color = get_field('color_options');
-	$size = get_field('size_options');
-
+	$custom_title = get_field( 'title' );
+	$custom_link  = get_field( 'link' );
+	$body         = get_field( 'body_text' );
+	$color        = get_field( 'color_options' );
+	$size         = get_field( 'size_options' );
 	?>
 
-	<div <?php echo $anchor; ?> class="<?php echo esc_attr($class_name); ?> <?php echo esc_attr($size); ?>-callout-block">
-		<div class="callout-block-component <?php echo esc_attr($color); ?> <?php echo esc_attr($size); ?>">
+	<div <?php echo esc_attr( $anchor ); ?> class="<?php echo esc_attr( $class_name ); ?> <?php echo esc_attr( $size ); ?>-callout-block">
+		<div class="callout-block-component <?php echo esc_attr( $color ); ?> <?php echo esc_attr( $size ); ?>">
 			<div class="callout-block-title">
-				<h2><?php echo esc_html( $title ); ?></h2>
+				<h2><?php echo esc_html( $custom_title ); ?></h2>
 			</div>
 			<div class="callout-block-body">
 				<p><?php echo esc_html( $body ); ?></p>
 			</div>
 			<?php
-			if( $link ):
-				$link_url = $link['url'];
-				$link_title = $link['title'];
-				$link_target = isset($link['target']) ? $link['target'] : '_self';
+			if ( $custom_link ) :
+				$custom_link_url    = $custom_link['url'];
+				$custom_link_title  = $custom_link['title'];
+				$custom_link_target = isset( $custom_link['target'] ) ? $custom_link['target'] : '_self';
 				?>
 				<div class="callout-block-link">
-					<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+					<a class="button" href="<?php echo esc_url( $custom_link_url ); ?>" target="<?php echo esc_attr( $custom_link_target ); ?>"><?php echo esc_html( $custom_link_title ); ?></a>
 				</div>
 			<?php endif; ?>
 		</div>
-		<?php if($size == 'large') : ?>
-			<div class="callout-wave">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/callout-wave-<?php echo esc_attr( $color ); ?>.svg" alt="" />
-			</div>
-		<?php endif; ?>
 	</div>
 <?php endif; ?>
