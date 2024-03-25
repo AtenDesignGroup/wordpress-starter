fb789444754fa2a16b6036869e03cbdfabba93d4
Elise Frigoli <elise.frigoli@gmail.com>
2024-03-25 10:11:33 -0500

    Refactoring Event Location block

--- wp-content/themes/aten-fse/blocks/event-location/event-location-block.php
+++ wp-content/themes/aten-fse/blocks/event-location/event-location-block.php
@@ -36,13 +36,13 @@ else :
 	if ( $location_post ) :
 		$location_post_id = $location_post->ID;
 		?>
-		<div <?php echo $anchor; ?> class="<?php echo esc_attr( $class_name ); ?>">
+		<div <?php echo esc_attr( $anchor ); ?> class="<?php echo esc_attr( $class_name ); ?>">
 			<div class="locations-block-component">
 				<?php
-					// Output the location post in the style of the Locations block
+					// Output the location post in the style of the Locations block.
 					echo '<ul class="location-blocks">';
 
-						// Get fields from the block
+						// Get fields from the block.
 						$phone              = get_field( 'phone' );
 						$address            = get_sub_field( 'address' );
 						$city               = get_sub_field( 'city' );
@@ -51,12 +51,14 @@ else :
 						$hours_of_operation = get_sub_field( 'hours_of_operation' );
 						$info               = get_sub_field( 'info' );
 						$image              = wp_get_attachment_image( get_sub_field( 'image' ), 'location-image' );
-						$location_title     = $location_link = $maps_link = '';
+						$location_title     = '';
+						$location_link      = '';
+						$maps_link          = '';
 
 						$location_link  = get_permalink( $location_post_id );
 						$location_title = get_the_title( $location_post_id );
 
-						// Getting default information for the location from the post's ACF fields
+						// Getting default information for the location from the post's ACF fields.
 						$default_phone   = get_field( 'contact_info_phone_number', $location_post_id );
 						$default_address = get_field( 'contact_info_address', $location_post_id );
 						$default_city    = get_field( 'contact_info_city', $location_post_id );
@@ -65,7 +67,7 @@ else :
 						$default_hours   = get_field( 'contact_info_hours_of_operation', $location_post_id );
 						$default_info    = get_field( 'contact_info_additional_information', $location_post_id );
 
-						// Defaulting to Location post values unless overridden by ACF content entered in block
+						// Defaulting to Location post values unless overridden by ACF content entered in block.
 						$phone   = empty( $phone ) ? esc_html( $default_phone ) : esc_html( $phone );
 						$address = empty( $address ) ? esc_html( $default_address ) : esc_html( $address );
 						$city    = empty( $city ) ? esc_html( $default_city ) : esc_html( $city );
@@ -78,113 +80,112 @@ else :
 					$hours_of_operation    = $default_hours;
 				}
 
-						// Generating Google Maps link if all pieces of address are present
+				// Generating Google Maps link if all pieces of address are present.
 				if ( $address && $city && $state && $zip ) {
-					// Concat the entire address string
+					// Concat the entire address string.
 					$concat_address = $address . ' ' . $city . ' ' . $state . ' ' . $zip;
-					// Replace whitespace for Google to handle it properly
+					// Replace whitespace for Google to handle it properly.
 					$concat_address = preg_replace( '/\s+/', '+', $concat_address );
 					$maps_link      = 'https://www.google.com/maps/place/' . $concat_address;
 				}
 
-						// Locations need to have some type of information in order to display
+				// Locations need to have some type of information in order to display.
 				if ( $location_title || $location_link || $phone || $address || $hours_of_operation || $info ) :
 					?>
-						
-							<li class="location-item">
-								<!-- Info Block -->
-								<div class="location-information">
-									<h2 class="location-block-title"><a href="<?php echo $location_link; ?>"><?php echo $location_title; ?></a></h2>
-									<ul class="location-details">
-										<?php
-
-										// Phone
-										if ( $phone ) :
-											?>
-											<li class="location-phone">
-												<span class="contact-icon call notranslate" aria-hidden="true">call</span><span class="a11y-visible">Phone Number </span>
-												<a href="tel:<?php echo $phone; ?>" title="Call">
-													<?php if ( strlen( $phone ) === 10 ) { ?>
-														(<?php echo substr( $phone, 0, 3 ); ?>) <?php echo substr( $phone, 2, 3 ); ?>-<?php echo substr( $phone, 5, 4 ); ?>
-														<?php
-													} else {
-														echo $phone; }
-													?>
-												</a>
-											</li>
-											<?php
-										endif;
-
-										// Address
-										if ( $address || $city || $zip ) :
-											?>
-											<li class="location-address">
-												<span class="contact-icon address notranslate" aria-hidden="true">location_on</span><span class="a11y-visible">Street Address </span>
-												<?php if ( $maps_link ) { ?>
-													<a href="<?php echo $maps_link; ?>" target="_blank" title="Get directions">
-												<?php } ?>
-														<?php echo ( $address . ', ' . $city . ', ' . $state . ' ' . $zip ); ?>
-												<?php if ( $maps_link ) { ?>
-													</a>
-												<?php } ?>
-											</li>
-											<?php
-										endif;
-
-										// Hours
-										if ( is_array( $hours_of_operation ) ) :
-											$hours = '';
-											?>
-											<li class="location-hours">
-												<span class="contact-icon hours notranslate" aria-hidden="true">schedule</span><span class="a11y-visible">Operating Hours </span>
-												<span class="hour-entries">Hours:
-												<?php
-												foreach ( $hours_of_operation as $hours_entry ) {
-													$days       = $hours_entry['days'];
-													$start_time = $hours_entry['start_time'];
-													$end_time   = $hours_entry['end_time'];
-
-													if ( $days && $start_time && $end_time ) {
-														$hours .= $days . ' ' . $start_time . '-' . $end_time . '; ';
-													}
-												}
-
-												// Removing trailing semicolon
-												echo ( substr( $hours, 0, -2 ) );
-												?>
-											</li>
-											<?php
-										endif; // Hours
-
-										// Additional Information
-										if ( $info ) :
-											?>
-											<li class="location-info">
-												<span class="contact-icon info notranslate" aria-hidden="true">info</span><span class="a11y-visible">Additional Information </span>
-												<?php echo esc_html( $info ); ?>
-											</li>
-										<?php endif; ?>
-									</ul>
-								</div>
-
-								<!-- Image Block -->
-								<div class="location-image 
+					<li class="location-item">
+						<!-- Info Block -->
+						<div class="location-information">
+							<h2 class="location-block-title"><a href="<?php echo esc_html( $location_link ); ?>"><?php echo esc_html( $location_title ); ?></a></h2>
+							<ul class="location-details">
 								<?php
-								if ( ! $image ) {
-									echo 'default-image';
-								} else {
-									echo 'custom-image'; }
-								?>
-								">
+
+								// Phone.
+								if ( $phone ) :
+									?>
+									<li class="location-phone">
+										<span class="contact-icon call notranslate" aria-hidden="true">call</span><span class="a11y-visible">Phone Number </span>
+										<a href="tel:<?php echo esc_html( $phone ); ?>" title="Call">
+											<?php if ( strlen( $phone ) === 10 ) { ?>
+												(<?php echo esc_html( substr( $phone, 0, 3 ) ); ?>) <?php echo esc_html( substr( $phone, 2, 3 ) ); ?>-<?php echo esc_html( substr( $phone, 5, 4 ) ); ?>
+												<?php
+											} else {
+												echo esc_html( $phone ); }
+											?>
+										</a>
+									</li>
 									<?php
-									if ( $image ) :
-										echo $image;
-									elseif ( ! $image ) :
+								endif;
+
+								// Address.
+								if ( $address || $city || $zip ) :
+									?>
+									<li class="location-address">
+										<span class="contact-icon address notranslate" aria-hidden="true">location_on</span><span class="a11y-visible">Street Address </span>
+										<?php if ( $maps_link ) { ?>
+											<a href="<?php echo esc_attr( $maps_link ); ?>" target="_blank" title="Get directions">
+										<?php } ?>
+												<?php echo esc_html( $address . ', ' . $city . ', ' . $state . ' ' . $zip ); ?>
+										<?php if ( $maps_link ) { ?>
+											</a>
+										<?php } ?>
+									</li>
+									<?php
+								endif;
+
+								// Hours.
+								if ( is_array( $hours_of_operation ) ) :
+									$hours = '';
+									?>
+									<li class="location-hours">
+										<span class="contact-icon hours notranslate" aria-hidden="true">schedule</span><span class="a11y-visible">Operating Hours </span>
+										<span class="hour-entries">Hours:
+										<?php
+										foreach ( $hours_of_operation as $hours_entry ) {
+											$days       = $hours_entry['days'];
+											$start_time = $hours_entry['start_time'];
+											$end_time   = $hours_entry['end_time'];
+
+											if ( $days && $start_time && $end_time ) {
+												$hours .= $days . ' ' . $start_time . '-' . $end_time . '; ';
+											}
+										}
+
+										// Removing trailing semicolon.
+										echo esc_html( substr( $hours, 0, -2 ) );
 										?>
-										<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/location_default.svg" alt="<?php echo $location_title; ?>" />
-									<?php endif; ?>
-								</div>
-							</li>
+									</li>
+									<?php
+								endif; // Hours.
+
+								// Additional Information.
+								if ( $info ) :
+									?>
+									<li class="location-info">
+										<span class="contact-icon info notranslate" aria-hidden="true">info</span><span class="a11y-visible">Additional Information </span>
+										<?php echo esc_html( $info ); ?>
+									</li>
+								<?php endif; ?>
+							</ul>
+						</div>
+
+						<!-- Image Block -->
+						<div class="location-image 
+						<?php
+						if ( ! $image ) {
+							echo 'default-image';
+						} else {
+							echo 'custom-image'; }
+						?>
+						">
+							<?php
+							if ( $image ) :
+								echo esc_html( $image );
+							elseif ( ! $image ) :
+								?>
+								<img src="/assets/icons/location_default.svg" alt="<?php echo esc_attr( $location_title ); ?>" />
+							<?php endif; ?>
+						</div>
+					</li>
 
 						<?php
 						endif;
