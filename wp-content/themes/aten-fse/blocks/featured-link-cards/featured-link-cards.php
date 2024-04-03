0677da5ffef8c263001263f8445007f6ce1078b3
Elise Frigoli <elise.frigoli@gmail.com>
2024-04-03 13:04:24 -0500

    Refactoring Featured Link Cards block

--- wp-content/themes/aten-fse/blocks/featured-link-cards/featured-link-cards.php
+++ wp-content/themes/aten-fse/blocks/featured-link-cards/featured-link-cards.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * @file
  * Featured Link Cards Template.
  *
  * @package aten-fse
@@ -33,19 +32,19 @@ else :
 		$class_name .= ' align' . $block['align'];
 	}
 
-	$title    = get_field( 'title' );
-	$subtitle = get_field( 'subtitle' );
+	$block_title = get_field( 'title' );
+	$subtitle    = get_field( 'subtitle' );
 
 	// Looping through repeater for Featured Link Cards.
 	if ( have_rows( 'featured_link_cards' ) ) : ?>
 
-		<div <?php echo $anchor; ?> class="<?php echo esc_attr( $class_name ); ?> featured-link-cards-block">
-			<img role="icon" alt="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/acf-icons/account_balance.svg" />
-			<h2 class="h2-stylized"><?php echo $title; ?></h2>
+		<div <?php echo esc_attr( $anchor ); ?> class="<?php echo esc_attr( $class_name ); ?> featured-link-cards-block">
+			<img role="icon" alt="" src="<?php echo esc_attr( get_stylesheet_directory_uri() ); ?>/assets/icons/acf-icons/account_balance.svg" />
+			<h2 class="h2-stylized"><?php echo esc_html( $block_title ); ?></h2>
 		<?php
 		if ( $subtitle ) {
 			?>
-		<p class="subtitle"><?php echo $subtitle; ?></p>
+		<p class="subtitle"><?php echo esc_html( $subtitle ); ?></p>
 			<?php
 		}
 		?>
@@ -57,21 +56,21 @@ else :
 					// Getting the subfield values.
 					$link_icon   = get_sub_field( 'link_icon' );
 					$link_title  = get_sub_field( 'link_title' );
-					$link        = get_sub_field( 'link_url' );
-					$link_target = $link['target'] ?? '_self';
+					$link_url    = get_sub_field( 'link_url' );
+					$link_target = $link_url['target'] ?? '_self';
 					$description = get_sub_field( 'description' );
 					// Checking for all required fields, otherwise this throws an error.
-					if ( $link_icon && $link_title && $link && $description ) :
+					if ( $link_icon && $link_title && $link_url && $description ) :
 						?>
 					<li class="featured-link-card">
-						<span class="link-icon notranslate" aria-hidden="true"><?php echo $link_icon; ?></span>
+						<span class="link-icon notranslate" aria-hidden="true"><?php echo esc_html( $link_icon ); ?></span>
 						<div class="text-wrapper">
 							<h3>
-								<a href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>" target="<?php echo $link_target; ?>" class="card-link">
-									<?php echo $link_title; ?>
+								<a href="<?php echo esc_attr( $link_url['url'] ); ?>" title="<?php echo esc_attr( $link_url['title'] ); ?>" target="<?php echo esc_attr( $link_target ); ?>" class="card-link">
+									<?php echo esc_html( $link_title ); ?>
 								</a>
 							</h3>
-							<p><?php echo $description; ?></p>
+							<p><?php echo esc_html( $description ); ?></p>
 						</div>
 					</li>
 						<?php
