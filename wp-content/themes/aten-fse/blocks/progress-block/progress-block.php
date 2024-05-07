083294b88e2a0945a9119f4f1ddb183252f2d4fb
Elise Frigoli <elise.frigoli@gmail.com>
2024-05-07 15:56:03 -0500

    Updating Progress block

--- wp-content/themes/aten-fse/blocks/progress-block/progress-block.php
+++ wp-content/themes/aten-fse/blocks/progress-block/progress-block.php
@@ -2,7 +2,7 @@
 /**
  * Progress Block Template.
  *
- * @package aten-fse
+ * @package aten-fse.
  *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
@@ -13,7 +13,7 @@
  * @param   array $context The context provided to the block by the post or it's parent block.
  */
 
-if ( isset( $block['data']['preview_image'] ) ) :    /* rendering in inserter preview  */
+if ( isset( $block['data']['preview_image'] ) ) :    /* rendering in inserter preview. */
 	echo '<img src="' . esc_attr( $block['data']['preview_image'] ) . '" style="width:100%; height:auto;">';
 else :
 
@@ -31,58 +31,58 @@ else :
 	if ( ! empty( $block['align'] ) ) {
 		$class_name .= ' align' . $block['align'];
 	}
-	$title        = get_field( 'title' );
+	$block_title  = get_field( 'title' );
 	$subtitle     = get_field( 'subtitle' );
 	$column_count = count( get_field( 'column' ) );
 
-	// Loop through Column
+	// Loop through Column.
 	if ( have_rows( 'column' ) ) : ?>
-		<div <?php echo $anchor; ?>class="<?php echo esc_attr( $class_name ); ?> l-full">
+		<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?> l-full">
 			<div class="progress-block-component">
-				<h2><?php echo esc_html( $title ); ?></h2>
-				<?php if ( $subtitle ) : ?>
+				<h2><?php echo esc_html( $block_title ); ?></h2>
+					<?php if ( $subtitle ) : ?>
 					<p class="subtitle"><?php echo esc_html( $subtitle ); ?></p>
-				<?php endif; // Subtitle ?>
+				<?php endif; // Subtitle. ?>
 				<div class="progress-block-columns">
-					<ul class="progress-block-list-wrapper col-count-<?php echo $column_count; ?>">
-						<?php
-						while ( have_rows( 'column' ) ) :
-							the_row();
-							$section_title = get_sub_field( 'section_title' );
-							?>
+					<ul class="progress-block-list-wrapper col-count-<?php echo esc_attr( $column_count ); ?>">
+							<?php
+							while ( have_rows( 'column' ) ) :
+								the_row();
+								$section_title = get_sub_field( 'section_title' );
+								?>
 							<li class="progress-block-column">
 								<hr />
 
 								<!-- Column loop content -->
-								<h3><?php echo $section_title; ?></h3>
+								<h3><?php echo esc_html( $section_title ); ?></h3>
 
 								<ul class="progress-links">
 									<?php
 									while ( have_rows( 'link' ) ) :
 										the_row();
-										$link        = get_sub_field( 'link_url' );
-										$link_title  = get_sub_field( 'link_title' );
-										$link_icon   = get_sub_field( 'link_icon' );
-										$link_target = isset( $link['target'] ) ? $link['target'] : '_self';
+										$progress_link = get_sub_field( 'link_url' );
+										$link_title    = get_sub_field( 'link_title' );
+										$link_icon     = get_sub_field( 'link_icon' );
+										$link_target   = isset( $progress_link['target'] ) ? $progress_link['target'] : '_self';
 										?>
 										<li class="progress-link">
 											<!-- Link loop content -->
-											<a href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>" target="<?php echo $link_target; ?>">
-												<span class="link-icon notranslate" aria-hidden="true"><?php echo $link_icon; ?></span>
-												<?php echo $link_title; ?>
+											<a href="<?php echo esc_url( $progress_link['url'] ); ?>" title="<?php echo esc_attr( $progress_link['title'] ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+												<span class="link-icon notranslate" aria-hidden="true"><?php echo esc_html( $link_icon ); ?></span>
+												<?php echo esc_html( $link_title ); ?>
 											</a>
 										</li>
-									<?php endwhile; // Link loop ?>
+									<?php endwhile; // Link loop. ?>
 								</ul>
 							</li>
-						<?php endwhile; // Column loop ?>
+							<?php endwhile; // Column loop. ?>
 					</ul>
 
 				</div>
 			</div>
 		</div>
 		<div class="progress-block-border" aria-hidden="true">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/progress-border.svg" alt="" />
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/progress-border.svg" alt="" />
 		</div>
-	<?php endif; // If Columns
+		<?php endif; // If Columns.
 endif; ?>
