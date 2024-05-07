46eb562b90d3d57451973cdfd60f3aa9c3052a16
Elise Frigoli <elise.frigoli@gmail.com>
2024-05-07 16:11:59 -0500

    Updating Resource block

--- wp-content/themes/aten-fse/blocks/resource-block/resource-block.php
+++ wp-content/themes/aten-fse/blocks/resource-block/resource-block.php
@@ -2,7 +2,7 @@
 /**
  * Resources Block Template.
  *
- * @package aten-fse
+ * @package aten-fse.
  *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
@@ -13,7 +13,7 @@
  * @param   array $context The context provided to the block by the post or it's parent block.
  */
 
-if ( isset( $block['data']['preview_image'] ) ) :    /* rendering in inserter preview  */
+if ( isset( $block['data']['preview_image'] ) ) :    /* rendering in inserter preview. */
 	echo '<img src="' . esc_attr( $block['data']['preview_image'] ) . '" style="width:100%; height:auto;">';
 else :
 
@@ -32,50 +32,51 @@ else :
 		$class_name .= ' align' . $block['align'];
 	}
 
-	// Looping through repeater for Column Blocks
+	// Looping through repeater for Column Blocks.
 	if ( have_rows( 'resources' ) ) : ?>
 
-		<div <?php echo $anchor; ?>class="<?php echo esc_attr( $class_name ); ?>">
+		<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>">
 			<div class="resources-block-wrapper">
 				<ul>
 					<?php
 					while ( have_rows( 'resources' ) ) :
 						the_row();
-						// Getting the subfield values
-						$title         = get_sub_field( 'title' );
-						$description   = get_sub_field( 'description' );
-						$file          = get_sub_field( 'file' );
-						$link          = get_sub_field( 'link' );
-						$has_file_type = $resource_url = '';
+						// Getting the subfield values.
+						$resource_title = get_sub_field( 'title' );
+						$description    = get_sub_field( 'description' );
+						$file           = get_sub_field( 'file' );
+						$resource_link  = get_sub_field( 'link' );
+						$has_file_type  = '';
+						$resource_url   = '';
 
 						if ( ! $file ) {
-							if ( is_array( $link ) ) {
-								$resource_url = $link['url'];
+							if ( is_array( $resource_link ) ) {
+								$resource_url = $resource_link['url'];
 							} else {
-								$resource_url = $link;
+								$resource_url = $resource_link;
 							}
 						} else {
 							$resource_url = $file['url'];
-							// Check if the resource has a file type
+							// Check if the resource has a file type.
 							$has_file_type = ! empty( get_post_mime_type( $file['ID'] ) );
 						}
 
-						// Output the title as a link
+						// Output the title as a link.
 						echo '<li class="resource">';
-						echo '<a href="' . esc_url( $resource_url ) . '"><h2>' . $title . '</h2></a>';
+						echo '<a href="' . esc_url( $resource_url ) . '"><h2>' . esc_html( $resource_title ) . '</h2></a>';
 
-						// Output the description text
+						// Output the description text.
 						if ( $description ) {
-							echo '<p>' . $description . '</p>';
+							echo '<p>' . esc_html( $description ) . '</p>';
 						}
 						echo '<p class="resource-details">';
 
-						// Output file type icon, file size, and updated date
+						// Output file type icon, file size, and updated date.
 						if ( $has_file_type ) {
-							// Get the file size in bytes
+							// Get the file size in bytes.
 							$file_size = $file['filesize'];
 
-							// Convert file size to kilobytes (KB)
+							// Convert file size to kilobytes (KB).
 							$file_size        = round( $file_size / 1024, 2 );
 							$file_size_suffix = 'KB';
 
@@ -84,14 +85,14 @@ else :
 								$file_size_suffix = 'MB';
 							}
 
-							// Get the file type extension
+							// Get the file type extension.
 							$filetype = ( wp_check_filetype( $file['filename'] ) );
 
-							// Get the updated date
+							// Get the updated date.
 							$updated_date = get_the_modified_date( 'm/d/y', $file['ID'] );
 
-							// Output file type icon, file size, and updated date
-							echo '<span class="resource-icon notranslate" aria-hidden="true">description</span> ' . $file_size . ' ' . $file_size_suffix . ' ' . strtoupper( $filetype['ext'] ) . ' | Updated ' . $updated_date;
+							// Output file type icon, file size, and updated date.
+							echo '<span class="resource-icon notranslate" aria-hidden="true">description</span> ' . esc_html( $file_size ) . ' ' . esc_html( $file_size_suffix ) . ' ' . esc_html( strtoupper( $filetype['ext'] ) ) . ' | Updated ' . esc_html( $updated_date );
 
 
 						} else {
