d38da017106ecef928b6e635249ed05cd951dac3
Elise Frigoli <elise.frigoli@gmail.com>
2024-06-05 13:10:47 -0500

    Renaming All Services block to Jump Link Card Directory

--- wp-content/themes/aten-fse/blocks/jump-link-directory-block/jump-link-directory-block.php
+++ wp-content/themes/aten-fse/blocks/jump-link-directory-block/jump-link-directory-block.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * All Services Block Template.
+ * Jump Link Directory Block Template.
  *
  * @package aten-fse
  *
@@ -24,7 +24,7 @@ else :
 	}
 
 	// Create class attribute allowing for custom "className" and "align" values.
-	$class_name = 'all-services-block includes-jump-links';
+	$class_name = 'jump-link-directory-block includes-jump-links';
 	if ( ! empty( $block['className'] ) ) {
 		$class_name .= ' ' . $block['className'];
 	}
@@ -32,57 +32,57 @@ else :
 		$class_name .= ' align' . $block['align'];
 	}
 
-	// Get the services field.
-	$services = get_field( 'services' );
+	// Get the cards field.
+	$cards = get_field( 'cards' );
 
-	// Loop through services sections if there are any.
-	if ( have_rows( 'services_sections' ) ) : ?>
+	// Loop through card sections if there are any.
+	if ( have_rows( 'card_sections' ) ) : ?>
 
 		<div <?php echo esc_attr( $anchor ); ?> class="<?php echo esc_attr( $class_name ); ?> l-gutter">
-			<a id="all-services-top-link"></a>
-			<h2>All Services</h2>
-			<h3><span class="services-icon jump-link-icon notranslate" aria-hidden="true">arrow_circle_down</span>Jump to a section of services:</h3>
-			<div class="services-jump-link-wrapper"></div>
+			<a id="jump-link-directory-block-top-link"></a>
+			<h2>All Cards</h2>
+			<h3><span class="jump-link-cards-icon jump-link-icon notranslate" aria-hidden="true">arrow_circle_down</span>Jump to a section of cards:</h3>
+			<div class="cards-jump-link-wrapper"></div>
 
-			<ul class="section-list">
+			<ul class="card-list">
 
 					<?php
 					// Counter to determine section card colors.
 					$color_counter = 1;
-					while ( have_rows( 'services_sections' ) ) :
+					while ( have_rows( 'card_sections' ) ) :
 						the_row();
 						// Get the section title.
 						$section_title = get_sub_field( 'section_title' );
 						?>
-					<li class="services-section">
-						<h4 class="service-section-heading"><?php echo esc_html( $section_title ); ?></h4>
-						<a href="#all-services-top-link" class="a11y-visible skip-link">Scroll back to Services List</a>
+					<li class="cards-section">
+						<h4 class="jump-link-card-section-heading"><?php echo esc_html( $section_title ); ?></h4>
+						<a href="#jump-link-directory-block-top-link" class="a11y-visible skip-link">Scroll back to Card List</a>
 
-						<?php if ( have_rows( 'services' ) ) : ?>
-							<ul class="service-cards">
+						<?php if ( have_rows( 'cards' ) ) : ?>
+							<ul class="jump-link-cards">
 								<?php
-								while ( have_rows( 'services' ) ) :
+								while ( have_rows( 'cards' ) ) :
 									the_row();
-									// Individual service field vars.
-									$service_title       = get_sub_field( 'service_title' );
-									$service_link        = get_sub_field( 'service_link' );
-									$service_link_target = $service_link['target'] ?? '_self';
-									$icon                = get_sub_field( 'service_icon' );
+									// Individual card field vars.
+									$jump_link_card_title       = get_sub_field( 'jump_link_card_title' );
+									$jump_link_card_link        = get_sub_field( 'jump_link_card_link' );
+									$jump_link_card_link_target = $jump_link_card_link['target'] ?? '_self';
+									$icon                = get_sub_field( 'jump_link_card_icon' );
 									// Optional.
-									$description = get_sub_field( 'service_description' );
+									$description = get_sub_field( 'jump_link_card_description' );
 									?>
-									<li class="service-card">
+									<li class="jump-link-card">
 										<div class="icon-container color-<?php echo esc_attr( $color_counter ); ?>">
-											<span class="service-icon notranslate" aria-hidden="true"><?php echo esc_html( $icon ); ?></span>
+											<span class="jump-link-card-icon notranslate" aria-hidden="true"><?php echo esc_html( $icon ); ?></span>
 										</div>
-										<div class="service-details">
-											<p class="service-title">
-												<a href="<?php echo esc_url( $service_link['url'] ); ?>" title="<?php echo esc_attr( $service_link['title'] ); ?>" target="<?php echo esc_attr( $service_link_target ); ?>" class="service-card-link">
-													<?php echo esc_html( $service_title ); ?>
+										<div class="jump-link-card-details">
+											<p class="jump-link-card-title">
+												<a href="<?php echo esc_url( $jump_link_card_link['url'] ); ?>" title="<?php echo esc_attr( $jump_link_card_link['title'] ); ?>" target="<?php echo esc_attr( $jump_link_card_link_target ); ?>" class="jump-link-card-link">
+													<?php echo esc_html( $jump_link_card_title ); ?>
 												</a>
 											</p>
 											<?php if ( $description ) : ?>
-												<p class="service-description">
+												<p class="jump-link-card-description">
 												<?php echo esc_html( $description ); ?>
 												</p>
 												<?php
@@ -111,7 +111,7 @@ else :
 
 			</ul>
 
-			<a class="back-to-top-btn" href="#all-services-top-link">
+			<a class="back-to-top-btn" href="#jump-link-directory-block-top-link">
 				<span class="arrow-icon notranslate" aria-hidden="true">arrow_upward</span> Back to Top
 			</a>
 		</div>
