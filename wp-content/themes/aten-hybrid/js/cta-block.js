2ee80256f0d74de641277fd5e60100b0bfbccdf4
Jennifer Dust <dust.jennifer@gmail.com>
2024-06-19 09:40:47 -0500

    Removing old build JS files

--- wp-content/themes/aten-hybrid/js/cta-block.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(n){var e=window.location.host;n(".cta-button").each(function(){var t=new URL(n(this).attr("href")).hostname,a="_blank"===n(this).attr("target");t===e&&!0!=a||n(this).addClass("external-link")})});
