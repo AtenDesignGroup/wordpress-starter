4cdb585f629c1fdf42f0b9539a47980827f64a44
Elise Frigoli <elise.frigoli@gmail.com>
2025-09-10 10:25:48 -0500

    ADGWP-Plugins: Adding custom plugin template to repo

---
+++ wp-content/plugins/aten-custom-plugin/aten-custom-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Aten Custom Plugin
+ * Description:       This is a custom plugin template for building custom functionality into a WordPress site.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            Aten Design Group
+ * Author URI:        https://atendesigngroup.com/
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package           Aten_Custom_Plugin
+ */
+
+// Load the required resources.
+require_once plugin_dir_path( __FILE__ ) . 'inc/example-plugin-functions.php';
